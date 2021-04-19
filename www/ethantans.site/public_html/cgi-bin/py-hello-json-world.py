81e37464684f159659453f69e649173f738e6c66
Ethan Tan <ethanyr@gmail.com>
2021-04-19 15:38:13 -0700

    json

---
+++ www/ethantans.site/public_html/cgi-bin/py-hello-json-world.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import socket
+import json
+from datetime improt date
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+today = date.today()
+time = today.strftime("%d:%m:%Y")
+thisdate = "current date is " + time
+obj = {"message":"Hello World from Python!", "date": thisdate, "ipAddress": ip}
+realobj = json.dumps(obj)
+print("Content-type: text/html\n")
+print('<html>')
+print('<head>')
+print('<title>Hello Python (JSON)!</title>')
+print('</head>')
+print('<body>')
+print(obj)
+print('</body>')
+print('</html>')
