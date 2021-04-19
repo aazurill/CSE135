7c9bd74303c231919e451c59350fe4d01e522be3
Ethan Tan <ethanyr@gmail.com>
2021-04-18 21:54:40 -0700

    first php

---
+++ www/ethantans.site/public_html/cgi-bin/php-hello-html-world.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Hello HTML World</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>";
+            echo "<p>Current Time: $date</p>";
+            $ip_address = gethostbyname("https://ethantans.site");
+            echo "<p>Your IP Address: $ip_address</p>";
+        ?>
+    </body>
+</html>
