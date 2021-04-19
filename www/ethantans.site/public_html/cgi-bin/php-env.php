4e92856e3131d4fffc8a77ba410d85221bf29161
Ethan Tan <ethanyr@gmail.com>
2021-04-19 12:26:40 -0700

    query

--- www/ethantans.site/public_html/cgi-bin/php-env.php
+++ www/ethantans.site/public_html/cgi-bin/php-env.php
@@ -5,11 +5,11 @@
           $env = getenv();
           echo"<h1>Environment Variables</h1>";
           foreach($env as $envir => $value) {
-            echo "<p>$envir: $value <br />\n<p>";
+            echo "<p><strong>$envir</strong>: $value <br />\n<p>";
           }
           echo "<h1>Server Variables</h1>";
           foreach ($headers as $header => $value) {
-              echo "<p>$header: $value <br />\n<p>";
+              echo "<p><strong>$header</strong>: $value <br />\n<p>";
           }
         ?>
     </body>
---
+++ www/ethantans.site/public_html/cgi-bin/php-get-echo.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <?php
+          echo "<p><b>Query String</b>$_SERVER['QUERY_STRING']</p>";
+        ?>
+    </body>
+</html>
