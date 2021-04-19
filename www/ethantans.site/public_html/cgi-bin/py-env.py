d92eb63b83c863cdd6817dcdae92ab368f456bb8
Ethan Tan <ethanyr@gmail.com>
2021-04-19 15:50:30 -0700

    env

---
+++ www/ethantans.site/public_html/cgi-bin/py-env.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+print("Content-type: text/html\n")
+print('<html>')
+print('<head>')
+print('<title>Env Variables</title>')
+print('</head>')
+print('<body>')
+print('<h2>Environment Variables</h2>')
+for k, v in os.environ.items():
+    print(f'{k}={v}')
+print('<h2>Server Variables</h2>')
+print('</body>')
+print('</html>')
