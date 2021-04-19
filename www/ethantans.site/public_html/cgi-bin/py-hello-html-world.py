014262c483a35a8d0698ce61df628a34dae1d874
Ethan Tan <ethanyr@gmail.com>
2021-04-19 15:21:35 -0700

    python

--- www/ethantans.site/public_html/cgi-bin/py-hello-html-world.py
+++ www/ethantans.site/public_html/cgi-bin/py-hello-html-world.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
-print "Content-type:text/html\r\n\r\n"
-print '<html>'
-print '<head>'
-print '<title>Hello Python!</title>'
-print '</head>'
-print '<body>'
-print '<h2>Hello World!</h2>'
-print '</body>'
-print '</html>'
+print("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('<title>Hello Python!</title>')
+print('</head>')
+print('<body>')
+print('<h2>Hello World!</h2>')
+print('</body>')
+print('</html>')
