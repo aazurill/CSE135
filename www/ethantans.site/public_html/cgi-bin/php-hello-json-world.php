1add4c870593a014f1f2da958953acfdd98dde34
Ethan Tan <ethanyr@gmail.com>
2021-04-19 10:24:01 -0700

    php json

--- www/ethantans.site/public_html/cgi-bin/php-hello-json-world.php
+++ www/ethantans.site/public_html/cgi-bin/php-hello-json-world.php
@@ -1,10 +1,14 @@
+<html>
+    <body>
         <?php
-            $ip = $_SERVER['REMOTE_ADDR'];
-            date_default_timezone_set('America/Los_Angeles');
-            $date = date('m/d/Y h:i:s a', time());
-            $myObj->message = "Hello World from PHP";
-            $myObj->ipAddress = $ip;
-            $myObj->date = $date;
-            header('Content-Type: text/html');
-            echo $myObj;
+          $ip = $_SERVER['REMOTE_ADDR'];
+          date_default_timezone_set('America/Los_Angeles');
+          $date = date('m/d/Y h:i:s a', time());
+          $myObj->message = "Hello World from PHP";
+          $myObj->ipAddress = $ip;
+          $myObj->date = $date;
+          $json_output = json_decode($myObj, true);
+          echo $json_output;
         ?>
+    </body>
+</html>
