c31ebeed9547fe1bb905ca0ec612c85d43617e7b
Ethan Tan <ethanyr@gmail.com>
2021-04-19 10:15:19 -0700

    json

--- www/ethantans.site/public_html/cgi-bin/php-hello-html-world.php
+++ www/ethantans.site/public_html/cgi-bin/php-hello-html-world.php
@@ -4,8 +4,9 @@
     </head>
     <body>
         <?php
-          $ip = $_SERVER['REMOTE_ADDR'];
-          $date = date('m/d/Y h:i:s a', time());
+            $ip = $_SERVER['REMOTE_ADDR'];
+            date_default_timezone_set('America/Los_Angeles');
+            $date = date('m/d/Y h:i:s a', time());
             echo "<p>Hello World!</p>";
             echo "<p>Current Time: $date</p>";
             echo "<p>Your IP Address: $ip</p>";
--- www/ethantans.site/public_html/cgi-bin/php-hello-json-world.php
+++ www/ethantans.site/public_html/cgi-bin/php-hello-json-world.php
@@ -4,12 +4,14 @@
     </head>
     <body>
         <?php
-          $ip = $_SERVER['REMOTE_ADDR'];
-          $date = date('m/d/Y h:i:s a', time());
-          $myObj->message = "Hello World from PHP";
-          $myObj->ipAddress = $ip;
-          $myObj->date = $date;
-          echo $myObj;
+            $ip = $_SERVER['REMOTE_ADDR'];
+            date_default_timezone_set('America/Los_Angeles');
+            $date = date('m/d/Y h:i:s a', time());
+            $myObj->message = "Hello World from PHP";
+            $myObj->ipAddress = $ip;
+            $myObj->date = $date;
+            header('Content-Type: application/json');
+            echo $myObj;
         ?>
     </body>
 </html>
