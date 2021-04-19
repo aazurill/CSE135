b9d09c846b61bfbf49fa0f0151434e548fbafe50
Ethan Tan <ethanyr@gmail.com>
2021-04-18 22:11:15 -0700

    php

--- www/ethantans.site/public_html/cgi-bin/php-hello-html-world.php
+++ www/ethantans.site/public_html/cgi-bin/php-hello-html-world.php
@@ -4,9 +4,10 @@
     </head>
     <body>
         <?php
+          $ip = $_SERVER['REMOTE_ADDR'];
             echo "<p>Hello World!</p>";
             echo "<p>Current Time: </p>";
-            echo "<p>Your IP Address: </p>";
+            echo "<p>Your IP Address: $ip</p>";
         ?>
     </body>
 </html>
