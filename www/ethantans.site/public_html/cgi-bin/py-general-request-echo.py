130b091056817f4e67e48cb3c8afa4e3dcaa80ae
Ethan Tan <ethanyr@gmail.com>
2021-04-19 20:39:33 -0700

    hi

--- www/ethantans.site/public_html/cgi-bin/py-general-request-echo.py
+++ www/ethantans.site/public_html/cgi-bin/py-general-request-echo.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 import os
-envi = os.environ
-method = envi['REQUEST_METHOD']
-protocol = envi['SERVER_PROTOCOL']
-query = envi['QUERY_STRING']
-r = requests.get('https://ethantans.site/cgi-bin/py-general-request-echo.py')
-body = r.content
+import requests
+# envi = os.environ
+# r = requests.get('https://ethantans.site')
+# print(envi)
+# print(r.headers)
+# for k, v in os.environ.items():
+#     print(f'{k}={v}')
+# method = envi['REQUEST_METHOD']
+# protocol = envi['SERVER_PROTOCOL']
+# query = envi['QUERY_STRING']
+# r = requests.get('https://ethantans.site/cgi-bin/py-general-request-echo.py')
+# body = r.content
 print("Content-type: text/html\n")
+method = os.environ['REQUEST_METHOD']
+protocol = os.environ['SERVER_PROTOCOL']
+query = os.environ['QUERY_STRING']
+# body = ''
 print('<html>')
 print('<head>')
 print('<title>General Request Echo</title>')
 print('</head>')
 print('<body>')
-print('<h2>Server Variables</h2>')
-print('<p>Request Method:{method} </p>')
-print('<p>Protocol: {protocol}</p>')
-print('<p>Query: {query} </p>')
+print('<h2>General Request</h2>')
+print(f'<p>Request Method:{method} </p>')
+print(f'<p>Protocol: {protocol}</p>')
+print(f'<p>Query: {query} </p>')
 print('<p>Message Body: {body}</p>')
 print('</body>')
 print('</html>')
+
