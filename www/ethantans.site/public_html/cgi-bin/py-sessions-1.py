7d0f2c41dc5be810441274a4b13add0de75179b5
Ethan Tan <ethanyr@gmail.com>
2021-04-19 19:39:47 -0700

    stuff

--- www/ethantans.site/public_html/cgi-bin/py-sessions-1.py
+++ www/ethantans.site/public_html/cgi-bin/py-sessions-1.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-import os
+import cgi, cgitbform
+form = cgi.FieldStorage()
+user = form.getvalue('username')
 print("Content-type: text/html\n")
 print('<html>')
 print('<head>')
-print('<title>Env Variables</title>')
+print('<title>Sessions</title>')
 print('</head>')
 print('<body>')
-print('<h2>Server Variables</h2>')
-for k, v in os.environ.items():
-    print(f'{k}={v}')
-    print('<br>')
+print('<h2>Hello {user}</h2>')
 print('</body>')
 print('</html>')
