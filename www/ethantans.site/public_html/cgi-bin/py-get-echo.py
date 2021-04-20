4e4fc6d0e6c95f7f546b71ec169ef23a9778d89c
Ethan Tan <ethanyr@gmail.com>
2021-04-19 18:08:57 -0700

    hi

--- www/ethantans.site/public_html/cgi-bin/py-get-echo.py
+++ www/ethantans.site/public_html/cgi-bin/py-get-echo.py
@@ -1,10 +1,12 @@
-import requests
 #!/usr/bin/env python3
-print("Content-type: text/html\n")
+import requests
 r = requests.get("http://www.google.com")
 print("Content-type: text/html\n")
 print('<html>')
 print('<body>')
+print('<h1>Query String: </h1>')
+print('<p>')
 print(r.content)
+print('</p>')
 print('</body>')
 print('</html>')
--- www/ethantans.site/public_html/cgi-bin/py-post-echo.py
+++ www/ethantans.site/public_html/cgi-bin/py-post-echo.py
@@ -1,5 +1,5 @@
-import requests
 #!/usr/bin/env python3
+import requests
 print("Content-type: text/html\n")
 URL = "http://google.com"
 myObj = {'hihihi':'byebyebye'}
@@ -8,6 +8,8 @@ x = requests.post(url, data=myObj)
 print("Content-type: text/html\n")
 print('<html>')
 print('<body>')
+print('<p>')
 print(x.text)
+print('</p>')
 print('</body>')
 print('</html>')
