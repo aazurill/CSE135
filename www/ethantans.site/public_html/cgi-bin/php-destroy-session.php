4bb465ba8debc160b20532308bed6b9d2bfec578
Ethan Tan <ethanyr@gmail.com>
2021-04-22 20:17:31 -0700

    sess

---
+++ www/ethantans.site/public_html/cgi-bin/php-destroy-session.php
@@ -0,0 +1,11 @@
+<html>
+  <body>
+    <?php
+    ?>
+    <h1>Session Destroyed!</h1>
+    <a href="./php-sessions-2.php">Session Page 2</a><br/>
+    <a href="./php-sessions-1.php">Session Page 1</a><br/>
+    <a href="../php-cgiform.html">Php CGI Form</a><br />
+    </form>
+  </body>
+</html>
