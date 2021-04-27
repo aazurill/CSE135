b1a186c47e455bbf23744cc7ad9f063c94531774
Ethan Tan <ethanyr@gmail.com>
2021-04-26 19:43:19 -0700

    'hi'

---
+++ www/ethantans.site/public_html/README.md
@@ -0,0 +1,2 @@
+## Website link:
+https://ethantans.site/
--- www/ethantans.site/public_html/cgi-bin/py-hello-html-world.py
+++ www/ethantans.site/public_html/cgi-bin/py-hello-html-world.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+import socket
+import json
+from datetime import date
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+today = date.today()
+time = today.strftime("%d:%m:%Y")
+thisdate = "current date is " + time
 print("Content-type: text/html\n")
 print('<html>')
 print('<head>')
-print('<title>Hello Python!</title>')
+print('<title>Hello World from Python!</title>')
 print('</head>')
 print('<body>')
-print('<h2>Hello World!</h2>')
+print('<h2>Hello World from Python!</h2>')
+print(f'<p>{time}</p>')
+print(f'<p>{ip}</p>')
 print('</body>')
 print('</html>')
