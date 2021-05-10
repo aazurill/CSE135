dcfe1e182ae4e01e2e422b86c0715bd412732dbd
Ethan Tan <ethanyr@gmail.com>
2021-05-10 13:29:21 -0700

    hi

---
+++ www/ethantans.site/public_html/noScript.php
@@ -0,0 +1,22 @@
+<?php
+$url = 'http://ethantans.site/api/static/posts';
+$data = array('js' => 'NOT_ALLOWED');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) {
+
+}
+
+var_dump($result);
+
+
+?>
